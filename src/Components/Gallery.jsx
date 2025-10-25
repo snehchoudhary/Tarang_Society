51f19795f11126a148107315b641c5114206ea0d
snehchoudhary <161978369+snehchoudhary@users.noreply.github.com>
2025-10-25 18:48:41 +0530

    Update Gallery.jsx

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Gallery.css";
 import gallery01 from '../assets/gallery01.jpg'
-import gallery02 from '../assets/gallery01.jpg'
-import gallery03 from '../assets/gallery01.jpg'
-import gallery04 from '../assets/gallery01.jpg'
-import gallery05 from '../assets/gallery01.jpg'
-import gallery06 from '../assets/gallery01.jpg'
-import gallery07 from '../assets/gallery01.jpg'
-import gallery08 from '../assets/gallery01.jpg'
-import gallery09 from '../assets/gallery01.jpg'
-import gallery10 from '../assets/gallery01.jpg'
+import gallery02 from '../assets/gallery02.jpg'
+import gallery03 from '../assets/gallery03.jpg'
+import gallery04 from '../assets/gallery04.jpg'
+import gallery05 from '../assets/gallery05.jpg'
+import gallery06 from '../assets/gallery06.jpg'
+import gallery07 from '../assets/gallery07.jpg'
+import gallery08 from '../assets/gallery08.jpg'
+import gallery09 from '../assets/gallery09.jpg'
+import gallery10 from '../assets/gallery10.jpg'
 
 
 const galleryImages = [
